4305650886983f19f56582d9b6841dd3ded256c4
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2025-07-07 17:05:52 -0500

    Avances Storage

--- 03-storage/main.tf
+++ 03-storage/main.tf
@@ -4,6 +4,7 @@ terraform {
       source = "hashicorp/azurerm"
       version = "4.35.0"
     }
+
   }
 }
 
@@ -15,4 +16,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
   location = "eastus"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name = var.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_tier = "Standard"
+  account_replication_type = "GRS"
+  access_tier = "Standard"
+  tags = {
+    environment = "Development"
+  }
 }
--- 03-storage/variables.tf
+++ 03-storage/variables.tf
@@ -2,6 +2,9 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "storage_account_name" {
+  type = string
+}
 
 variable "suscription_id" {
   type = string
